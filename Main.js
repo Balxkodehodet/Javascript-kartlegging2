9a3c588a42ce59e3114ecd2a2f03c6cabd9af13c
Balxkodehodet <balx042025@gmail.com>
2025-04-23 12:15:58 +0200

    oppdatert requestAnimationFrame

--- Main.js
+++ Main.js
@@ -1,61 +1,68 @@
 const togglebtn = document.getElementById("toggle-btn");
 
-let today = new Date();
+function UpdateTime() {
+  let today = new Date();
 
-let day = today.getDay();
+  let day = today.getDay();
 
-let hour = today.getHours();
-let minute = today.getMinutes();
-let second = today.getSeconds();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
 
-let IDtoday = document.getElementById("Date");
+  let IDtoday = document.getElementById("Date");
 
-const daylist = [
-  "Søndag",
-  "Mandag",
-  "Tirsdag",
-  "Onsdag",
-  "Torsdag",
-  "Fredag",
-  "Lørdag",
-];
+  const daylist = [
+    "Søndag",
+    "Mandag",
+    "Tirsdag",
+    "Onsdag",
+    "Torsdag",
+    "Fredag",
+    "Lørdag",
+  ];
 
-let textcontent = (IDtoday.textContent = "Idag er " + daylist[day] + ".");
+  let textcontent = (IDtoday.textContent = "Idag er " + daylist[day] + ".");
 
-let prepand = hour >= 12 ? " PM " : " AM ";
+  let prepand = hour >= 12 ? " PM " : " AM ";
 
-hour = hour >= 12 ? hour - 12 : hour;
+  hour = hour >= 12 ? hour - 12 : hour;
 
-if (hour === 0 && prepand === " PM ") {
-  if (minute === 0 && second === 0) {
-    hour = 12;
-    prepand = " Noon ";
-  } else {
-    hour = 12;
-    prepand = " PM ";
+  if (hour === 0 && prepand === " PM ") {
+    if (minute === 0 && second === 0) {
+      hour = 12;
+      prepand = " Noon ";
+    } else {
+      hour = 12;
+      prepand = " PM ";
+    }
   }
+
+  //Check for  special cases when hour is 0
+  if (hour === 0 && prepand === " AM ") {
+    if (minute === 0 && second === 0) {
+      hour = 12;
+      prepand = " Midnight";
+    } else {
+      hour = 12;
+      prepand = " AM";
+    }
+  }
+
+  IDtoday.textContent =
+    textcontent +
+    " Nåværende tid: " +
+    hour +
+    prepand +
+    " : " +
+    minute +
+    " : " +
+    second;
+
+  // Call this function again on the next frame
+  requestAnimationFrame(UpdateTime);
 }
 
-//Check for  special cases when hour is 0
-if (hour === 0 && prepand === " AM ") {
-  if (minute === 0 && second === 0) {
-    hour = 12;
-    prepand = " Midnight";
-  } else {
-    hour = 12;
-    prepand = " AM";
-  }
-}
-
-IDtoday.textContent =
-  textcontent +
-  " Nåværende tid: " +
-  hour +
-  prepand +
-  " : " +
-  minute +
-  " : " +
-  second;
+requestAnimationFrame(UpdateTime);
 
 function themeSwitcher() {
   document.body.classList.toggle("dark-mode");
